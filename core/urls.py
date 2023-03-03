b027be3ec74168d79458fa73e7f43fbaf21198d9
Romulo P T Mota <romuloptmota@gmail.com>
2023-03-03 00:17:20 -0300

    Criado template Locatarios|Clientes

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, ClientesView
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index')
+    path('', IndexView.as_view(), name='index'),
+    path('clientes', ClientesView.as_view(), name='clientes')
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+
+class ClientesView(TemplateView):
+    template_name = 'clientes.html'
