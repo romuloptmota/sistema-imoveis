ec98e51f64bbb0e2a51788e90bc7d2c713c2de74
Romulo P T Mota <romuloptmota@gmail.com>
2023-03-03 14:31:12 -0300

    Criado Models e Admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Building, Apartament, Client
+
+
+@admin.register(Building)
+class BuildingAdmin(admin.ModelAdmin):
+    list_display = ('edificio',)
+
+
+@admin.register(Apartament)
+class ApartamentAdmin(admin.ModelAdmin):
+    list_display = ('edificio', 'apartamento', 'quartos', 'banheiros', 'vagas', 'tamanho', 'valor',
+                    'imagem1', 'imagem2', 'imagem3', 'imagem4', 'imagem5', 'disponivel', 'locatario')
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('cliente', 'email', 'telefone')
--- core/models.py
+++ core/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+from stdimage.models import StdImageField
+
+
+class Building(models.Model):
+    edificio = models.CharField('Edifício', max_length=100)
+
+    def __str__(self):
+        return self.edificio
+
+
+class Apartament(models.Model):
+    edificio = models.ForeignKey("Building", on_delete=models.PROTECT)
+    apartamento = models.DecimalField('Nº Apto', decimal_places=0, max_digits=10)
+    quartos = models.DecimalField('Nº de quartos', decimal_places=0, max_digits=2)
+    banheiros = models.DecimalField('Nº de banheiros', decimal_places=0, max_digits=2)
+    vagas = models.DecimalField('Nº de vagas', decimal_places=0, max_digits=2)
+    tamanho = models.DecimalField('m²', decimal_places=0, max_digits=4)
+    valor = models.DecimalField('Valor', decimal_places=0, max_digits=10)
+    imagem1 = StdImageField('Imagem 1', upload_to='media/apartamentos', variations={'thump': {'width': 457, 'height': 367, 'crop': True}})
+    imagem2 = StdImageField('Imagem 2', upload_to='media/apartamentos', variations={'thump': {'width': 457, 'height': 367, 'crop': True}})
+    imagem3 = StdImageField('Imagem 3', upload_to='media/apartamentos', variations={'thump': {'width': 457, 'height': 367, 'crop': True}})
+    imagem4 = StdImageField('Imagem 4', upload_to='media/apartamentos', variations={'thump': {'width': 457, 'height': 367, 'crop': True}})
+    imagem5 = StdImageField('Imagem 5', upload_to='media/apartamentos', variations={'thump': {'width': 457, 'height': 367, 'crop': True}})
+    disponivel = models.BooleanField('Disponivel?', default=True)
+    locatario = models.OneToOneField("Client", on_delete=models.PROTECT, blank=True, null=True)
+
+
+class Client(models.Model):
+    cliente = models.CharField('Locatario', max_length=100)
+    email = models.CharField('E-mail', max_length=100)
+    telefone = models.CharField('Telefone', max_length=20)
+
+    def __str__(self):
+        return self.cliente
