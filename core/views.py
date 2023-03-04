220fea9f1666124ba0021005fa394ceaf18f4df7
Romulo P T Mota <romuloptmota@gmail.com>
2023-03-04 03:07:03 -0300

    configurado não acesso cliente

--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 from django.views.generic import TemplateView
 from .models import Apartamento
 
+# controle de login
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 class IndexView(TemplateView):
     template_name = 'index.html'
@@ -13,7 +16,10 @@ class IndexView(TemplateView):
         return context
 
 
-class ClientesView(TemplateView):
+class ClientesView(LoginRequiredMixin, TemplateView):
+    #acesso somente logado - deslogado enviado para index
+    login_url = reverse_lazy('index')
+
     template_name = 'clientes.html'
 
     def get_context_data(self, **kwargs):
