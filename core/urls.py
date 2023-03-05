1ad006e6008c812587206028e73014fe2ed99a26
Romulo P T Mota <romuloptmota@gmail.com>
2023-03-04 23:42:04 -0300

    Correções pagina de cadstro

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,6 @@ from .views import IndexView, ClientesView, criar, banco
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('clientes', ClientesView.as_view(), name='clientes'),
-    path('criar/', criar),
+    path('criar/', criar, name='criar'),
     path('banco/', banco),
 ]
